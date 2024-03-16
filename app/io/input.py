1eaec27eac9d27d2a726905faabee55075e30a1b
natal <nataliia.malashok@ukma.edu.ua>
2024-03-16 15:46:38 +0200

    додано функції вводу та виводу

---
+++ app/io/input.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+
+def input_from_console():
+    '''
+    Function to get text from console.
+    '''
+    text = input('Enter text: ')
+    return text
+
+
+def input_from_file(file_path):
+    '''
+    Function to read from a file.
+
+    Parameters:
+        file_path: name of file
+
+    '''
+    try:
+        with open(file_path, 'r') as file:
+            res = file.read()
+            return res
+    except FileNotFoundError:
+        return FileNotFoundError
+
+
+def input_from_file_pd(file_path):
+    '''
+    Function for reading from a file using the pandas.
+
+    Parameters:
+        file_path: name of file
+
+    '''
+    try:
+        res = pd.read_txt(file_path)
+        return res
+    except FileNotFoundError:
+        return FileNotFoundError
---
+++ app/io/output.py
@@ -0,0 +1,24 @@
+def output_to_console(text):
+    '''
+    Function to print text in console.
+
+    Parameters:
+        text: text to print
+    '''
+    print(text)
+
+
+def output_to_file(file_path, text):
+    '''
+    Function to print text in file.
+
+    Parameters:
+        file_path: path to file
+        text: text to output
+    '''
+    try:
+        with open(file_path, 'w') as file:
+            file.write(text)
+        print(file_path)
+    except FileNotFoundError:
+        raise FileNotFoundError
