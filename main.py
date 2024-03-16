d6999202c20f32ce6cfebd64e6c585f421113838
natal <nataliia.malashok@ukma.edu.ua>
2024-03-16 16:06:53 +0200

    виклики функцій input та output в main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from app.io import input, output
+
 def main():
-    pass
+    from_console = input.input_from_console()
+    from_file = input.input_from_file("data/example.txt")
+    from_file_pd = input.input_from_file_pd("data/example.txt")
+    print()
+    output.output_to_console("from console: " + from_console)
+    output.output_to_console("from file: " + from_file)
+    output.output_to_console("from file 2 (pandas): " + str(from_file_pd))
+    print()
+    output.output_to_file("data/console_out.txt", from_console)
+    output.output_to_file("data/file_out.txt", from_file)
+    output.output_to_file("data/file_out_pd.txt", str(from_file_pd))
+
 
 if __name__ == "__main__":
     main()
