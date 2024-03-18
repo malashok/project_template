91da4f6b3f80c4d67c5f298ccd593592be93d20c
natal <nataliia.malashok@ukma.edu.ua>
2024-03-18 20:03:24 +0200

    додано 3 функцію output та її виклик в main

--- app/io/output.py
+++ app/io/output.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def output_to_console(text):
     '''
     Function to print text in console.
@@ -19,6 +22,22 @@ def output_to_file(file_path, text):
     try:
         with open(file_path, 'w') as file:
             file.write(text)
+        print(file_path)
+    except FileNotFoundError:
+        raise FileNotFoundError
+
+
+def output_to_file_pd(file_path, text):
+    '''
+    Function to write DataFrame to a CSV file.
+
+    Parameters:
+        file_path: path to file
+        text: dataframe to output
+    '''
+    try:
+        df = pd.DataFrame(text)
+        df.to_csv(file_path, index=False)
         print(file_path)
     except FileNotFoundError:
         raise FileNotFoundError
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from app.io import input, output
 
+
 def main():
     from_console = input.input_from_console()
     from_file = input.input_from_file("data/example.txt")
@@ -11,7 +12,7 @@ def main():
     print()
     output.output_to_file("data/console_out.txt", from_console)
     output.output_to_file("data/file_out.txt", from_file)
-    output.output_to_file("data/file_out_pd.txt", str(from_file_pd))
+    output.output_to_file_pd("data/file_out_pd.txt", from_file_pd)
 
 
 if __name__ == "__main__":
